6bf66282ebbfd384b493f3b291289e9ea39d9051
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_BindingToOlapCube/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace DXPivotGrid_BindingToOlapCube {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+        }
+    }
+}
