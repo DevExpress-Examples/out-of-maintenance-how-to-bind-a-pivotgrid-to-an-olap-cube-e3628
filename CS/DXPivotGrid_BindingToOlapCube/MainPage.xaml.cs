d4e6afcbf2b67dfc306de64c1268d0ea4642fc60
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 12:50:34 +0400

    Change example status to out of maintenance

--- CS/DXPivotGrid_BindingToOlapCube/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace DXPivotGrid_BindingToOlapCube {
-    public partial class MainPage : UserControl {
-        public MainPage() {
-            InitializeComponent();
-        }
-    }
-}
